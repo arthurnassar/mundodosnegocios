e1ae97f43eadd5db4191a2a8516015239a12f6a7
arthurnassar <arthurnassar@gmail.com>
2022-04-22 10:34:11 -0400

    versao 1

--- scripts/main.js
+++ scripts/main.js
@@ -29,40 +29,6 @@ function timeShowBody() {
 
 function startWebSite() {
   timeShowBody()
-  componetTimer()
-}
-
-function componetTimer() {
-
-  // TIMER 
-  var minutes = 14;
-  var seconds = 60;
-  const minutesTimer = document.querySelector('#min-counter');
-  const secondsTimer = document.querySelector('#seconds-counter');
-  // contador dos minutos
-  setInterval(() => {
-    if (minutes == 0) {
-      clearInterval();
-    }
-    else {
-      minutes--;
-      minutesTimer.innerHTML = "<b>" + minutes + "</b>";
-    }
-  }, 60000)
-  // contador dos minutos
-  setInterval(() => {
-    if (minutes == 0) {
-      clearInterval();
-    }
-    else if (seconds == 0) {
-      seconds = 59
-    }
-    else {
-      seconds--;
-      secondsTimer.innerHTML = "<b>" + seconds + "</b>";
-    }
-  }, 1000)
-
 }
 
 startWebSite()
